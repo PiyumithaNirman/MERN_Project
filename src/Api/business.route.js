be4ccbace9330f2df621308b488418c23622eb68
Gamage Computer <Gamage Computer@GAMAGECOMPUTER>
2020-07-04 14:34:24 +0530

    develop edit component

--- src/Api/business.route.js
+++ src/Api/business.route.js
@@ -1,45 +1,21 @@
 const express = require('express');
 const businessRoutes = express.Router();
+const mongoose = require("mongoose");
+const Business = require('./business.model');
 
-let Business = require('./business.model');
-
-// businessRoutes.route('./add').post(function(req,res){
-//     let business = new Business(req.body);
-//     business.save()
-//         .then(business => {
-//             res.status(200).json({'business': 'business is added successfully'});
-//         })
-//         .catch(err =>{
-//             res.status(400).send("unable to save to database");
-//         });
-//     });
 
 businessRoutes.post("/add", (req, res, next) => {
-        console.log("calling post API :"+ req.body.name);
-          const product = new Product({
-            _id: new mongoose.Types.ObjectId(),
-            person_name: req.body.person_name,
-            business_name: req.body.business_name,
-            business_nic_number: req.body.business_nic_number
-          });
-          console.log("calling post API");
-      
-          product
-            .save()
-            .then(result => {
-              console.log(result);
-              res.status(201).json({
-                message: "Handling POST requests to /products",
-                createdProduct: result
-              });
-            })
-            .catch(err => {
-              console.log(err);
-              res.status(500).json({
-                error: err
-              });
-            });
+        console.log("calling post API piyu post :"+ req.body.person_name);
+        const business = new Business(req.body);
+        console.log(req.body);
+    business.save()
+        .then(business => {
+            res.status(200).json({'business': 'business is added successfully'});
+        })
+        .catch(err =>{
+            res.status(400).send("unable to save to database");
         });
+    });
 
     businessRoutes.route('/').get(function (req, res){
         Business.find(function(err, business){
